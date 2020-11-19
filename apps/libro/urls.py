32a3680d94f02769ba5767b4bd42f9fcc59cfa03
Fernando <fernando.acosta216555@potros.itson.edu.mx>
2020-11-18 20:30:30 -0700

    Ultimo avance, le sigo mas tarde

--- apps/libro/urls.py
+++ apps/libro/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path
-from .views import crearAutor
+from .views import *
 
 urlpatterns = [
-    path('crear_autor/',crearAutor,name='crear_autor')
+    path('crear_autor/',crearAutor,name='crear_autor'),
+    path('listar_autor/',listarAutor,name='listar_autor'),
+    path('editar_autor/<int:id>',editarAutor,name='editar_autor')
+
+    
+
 ]
+
+
--- apps/libro/views.py
+++ apps/libro/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render,redirect
 from .forms import AutorForm
+from .models import *
+from django.core.exceptions import ObjectDoesNotExist
 # Create your views here.
 def Home(request):
     return render(request,'index.html')
@@ -16,3 +18,22 @@ def crearAutor(request):
         print(autor_form)
 
     return render(request,'libro/crear_autor.html',{'autor_form':autor_form})
+
+def listarAutor(request):
+    autores = Autor.objects.all()
+    return render(request,'libro/listar_autor.html',{'autores':autores})
+
+def editarAutor(request,id):
+    try:
+        autor = Autor.objects.get(id = id)
+        if request.method == 'GET':
+            autor_form = AutorForm(instance = autor)
+        else:
+            autor_form = AutorForm(request.POST,instance=autor)
+            if autor_form.is_valid():
+                autor_form.save()
+                return redirect('home')
+    except ObjectDoesNotExist as e:
+        error = e
+    
+        return render(request,'libro/crear_autor.html',{'error':error})
